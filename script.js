a58376a6fb2bad4982c96198626ae12941215074
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-05-08 23:45:08 +0200

    Controller running

--- script.js
+++ script.js
@@ -23,12 +23,55 @@ function loadSong(song) {
   cover.src = `images/${song}.jpg`;
 }
 
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+
+  audio.pause();
+}
+
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
 playBtn.addEventListener('click', () => {
-    const isPlaying = musicContainer.classList.contains('play');
-    
-    if (isPlaying) {
-        pauseSong();
-    } else {
-        playSong();
-    }
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
 });
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
